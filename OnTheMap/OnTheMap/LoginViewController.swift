d107f4d5542c03987bf8b386bbb1cb166bfe5801
Gamal Gamaleldin <gamal.m.esmail@gmail.com>
2019-02-12 12:44:23 +0200

    Related the login view with the login VC

---
+++ OnTheMap/OnTheMap/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  OnTheMap
+//
+//  Created by Gamal Gamaleldin on 2/12/19.
+//  Copyright © 2019 el3ankaboot. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    //MARK: Outlets
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signupButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
