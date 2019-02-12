8982e14e76ef163cd7c6058b37b36654e670ff12
Gamal Gamaleldin <gamal.m.esmail@gmail.com>
2019-02-12 19:58:45 +0200

    Relate tableview to its VC

---
+++ OnTheMap/OnTheMap/TableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TableViewController.swift
+//  OnTheMap
+//
+//  Created by Gamal Gamaleldin on 2/12/19.
+//  Copyright © 2019 el3ankaboot. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TableViewController : UIViewController , UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+    
+    
+}
