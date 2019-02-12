ce08ce77e37b6f8860a62f904c110d4503b4c49f
Gamal Gamaleldin <gamal.m.esmail@gmail.com>
2019-02-12 12:41:18 +0200

    Drew UI elements for login view

---
+++ OnTheMap/OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Gamal Gamaleldin on 2/12/19.
+//  Copyright © 2019 el3ankaboot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
